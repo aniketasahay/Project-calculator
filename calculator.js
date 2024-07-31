4f79916e8f8f2f92cd7fa7adb999cd43c65f7560
unknown <as.aniketa@gmail.com>
2024-07-31 14:26:35 +0530

    1st commit ever

---
+++ calculator.js
@@ -0,0 +1,29 @@
+console.log('im working');
+let currentDisplay = '';
+document.querySelector('#display').value = currentDisplay;
+
+// document.querySelectorAll('.button').onclick=" currentDisplay= currentDisplay + button; "
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(object =>{
+  object.addEventListener("click", ()=>{
+    buttonValue = object.value;
+    currentDisplay = currentDisplay+ buttonValue;
+    document.querySelector('#display').value = currentDisplay;
+  });
+});
+document.querySelector('#clear').addEventListener("click",()=>{
+  currentDisplay= '';
+  document.querySelector('#display').value = currentDisplay;
+});
+// document.querySelector('#sub').addEventListener("click",()=>{
+//   document.querySelector('#display').value = '';
+// });
+document.querySelector('#calc').addEventListener("click",()=>{
+   try{
+    currentDisplay= eval(currentDisplay);
+  document.querySelector('#display').value = currentDisplay;
+   } catch(error){
+    document.querySelector('#display').value = "error";
+   }
+});
+
